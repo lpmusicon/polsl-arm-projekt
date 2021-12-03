345ad2473628a6f7323c32b9f1b1770420b96265
Mateusz Bielecki <mateusz.bielecki@icloud.com>
2021-12-03 09:26:21 +0100

    LCD test

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'zl29arm_hd44780' 
+ * Target:  'zl29arm_hd44780' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/zl29arm_hd44780/startup_stm32f107xc.d
@@ -0,0 +1,1 @@
+zl29arm_hd44780\startup_stm32f107xc.o: startup_stm32f107xc.s
